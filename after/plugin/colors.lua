8c95520d8a554b367c36cd35ea5b7ca31033add0
Priyanshu Prasad Gupta <guptapriyanshu973@gmail.com>
2023-09-20 14:45:46 +0530

    yoooo

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,23 @@
 require('kanagawa').setup({
     transparent = true,
+    theme = "dragon",
+    background = {               -- map the value of 'background' option to a theme
+        dark = "dragon",           -- try "dragon" !
+--        light = "lotus"
+    },
+--    dim_nc_background = true,
+--	disable_background = true,
+--    disable_float_background = true,
 })
 
 function ColorMyPencils(color)
-	color = color or "kanagawa"
-	vim.cmd.colorscheme(color)
-  vim.cmd("hi LineNr guibg=NONE")
+    color = color or "kanagawa"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd("hi LineNr guibg=NONE")
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
--- lua/rootpalladium/packer.lua
+++ lua/rootpalladium/packer.lua
@@ -39,11 +39,7 @@ return require('packer').startup(function(use)
 				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
 				ts_update()
 			end,}
-  use("nvim-treesitter/playground")
-  use("theprimeagen/harpoon")
-  use("theprimeagen/refactoring.nvim")
   use("mbbill/undotree")
-  use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
 
   use {
@@ -68,10 +64,6 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 	  }
   }
-
-  use("folke/zen-mode.nvim")
-  use("eandrju/cellular-automaton.nvim")
-  use("laytan/cloak.nvim")
 
 end)
 
